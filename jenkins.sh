cd3d4bfbd083667d1de17a02973c897eda699eaa
Alekhyabotla <150768809+Alekhyabotla@users.noreply.github.com>
2025-07-18 13:25:26 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -3,3 +3,5 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install jenkins -y
 systemctl start jenkins
+systemctl enable jenkins
+systemctl status jenkins
